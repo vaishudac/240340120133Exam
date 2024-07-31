d7f215d019f3f506055a9fa8ebc772ef8245f443
vaishudac <vaishudac24@gmail.com>
2024-07-31 21:29:39 +0530

    version 1

---
+++ EvenNumber.java
@@ -0,0 +1,12 @@
+public class EvenNumbers {
+  public static void main(String[] args) {
+    int count = 0;
+    int num = 2;
+
+    while (count < 10) {
+      System.out.println(num);
+      num += 2;
+      count++;
+    }
+  }
+}
